16a196d388600defea85500274e49bd2f7463892
wwoszczek <wwoszczek@student.agh.edu.pl>
2021-12-11 12:46:47 +0100

    crossover v1.0 by Woszczu

--- Crossover.py
+++ Crossover.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 class CrossoverParameters:
     """
     Class for coherent parameter definition for Crossover operator.
@@ -44,21 +47,50 @@ class Crossover:
             return self.__cross_uniform(solution1, solution2)
 
     def __cross_one_point(self, solution1, solution2):
-        # TODO: Uzupełnić
-        return
+        if self.parameters.distribution_of_cut == "Uniform":
+            if solution1.shape != solution2.shape:
+                raise ValueError(f"Solution shapes {solution1.shape} and {solution2.shape} don't match!")
+            idx = np.random.randint(1, len(solution1))
+            x, y = solution1.copy(), solution2.copy()
+            x[idx:], y[idx:] = solution2[idx:], solution1[idx:]
+            return x, y
+        raise NotImplementedError(f"Crossing not implemented for {self.parameters.distribution_of_cut}!")
 
     def __cross_two_points(self, solution1, solution2):
-        # TODO: Uzupełnić
-        return
+        if self.parameters.distribution_of_cut == "Uniform":
+            if solution1.shape != solution2.shape:
+                raise ValueError(f"Solution shapes {solution1.shape} and {solution2.shape} don't match!")
+            idx1, idx2 = np.sort(np.random.choice(range(1, len(solution1)), size=2, replace=False))
+            x, y = solution1.copy(), solution2.copy()
+            x[idx1:idx2], y[idx1:idx2] = solution2[idx1:idx2], solution1[idx1:idx2]
+            return x, y
+        raise NotImplementedError(f"Crossing not implemented for {self.parameters.distribution_of_cut}!")
 
     def __cross_average(self, solution1, solution2):
-        # TODO: Uzupełnić
-        return
+        if self.parameters.distribution_of_cut == "Uniform":
+            if solution1.shape != solution2.shape:
+                raise ValueError(f"Solution shapes {solution1.shape} and {solution2.shape} don't match!")
+            x = (0.5 * solution1 + 0.5 * solution2).astype(int)
+            return x, x
+        raise NotImplementedError(f"Crossing not implemented for {self.parameters.distribution_of_cut}!")
 
     def __cross_convex(self, solution1, solution2):
-        # TODO: Uzupełnić
-        return
+        if self.parameters.distribution_of_cut == "Uniform":
+            if solution1.shape != solution2.shape:
+                raise ValueError(f"Solution shapes {solution1.shape} and {solution2.shape} don't match!")
+            alpha = np.random.rand()
+            x = (alpha * solution1 + (1 - alpha) * solution2).astype(int)
+            y = ((1 - alpha) * solution1 + alpha * solution2).astype(int)
+            return x, y
+        raise NotImplementedError(f"Crossing not implemented for {self.parameters.distribution_of_cut}!")
 
     def __cross_uniform(self, solution1, solution2):
-        # TODO: Uzupełnić
-        return
+        if self.parameters.distribution_of_cut == "Uniform":
+            if solution1.shape != solution2.shape:
+                raise ValueError(f"Solution shapes {solution1.shape} and {solution2.shape} don't match!")
+            pat = np.random.randint(2, size=len(solution1))
+            pat_neg = 1 - pat
+            x = (solution1 * pat + solution2 * pat_neg).astype(int)
+            y = (solution1 * pat_neg + solution2 * pat).astype(int)
+            return x, y
+        raise NotImplementedError(f"Crossing not implemented for {self.parameters.distribution_of_cut}!")
